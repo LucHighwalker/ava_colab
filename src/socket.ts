8322f219331b157f8ad3fd96af31dacc0b81576f
LucHighwalker <luc.highwalker@gmail.com>
2020-04-04 22:30:33 -0700

    [Fix] Server not broadcasting to all clients.

--- src/socket.ts
+++ src/socket.ts
@@ -3,16 +3,10 @@ import { Socket } from "socket.io";
 import mutationController from "./mutation/mutation.controller";
 
 export default (socket: Socket) => {
-	console.log("socket connected");
-
-	socket.on("message", (msg) => {
-		console.log("message received: ", msg);
-	});
-
 	socket.on("mutation", async (data) => {
 		try {
 			const { mutation } = await mutationController.create(data);
-			socket.emit(`mutationRecieved-${mutation.conversationId}`, mutation);
+			socket.broadcast.emit(`mutationRecieved-${mutation.conversationId}`, mutation);
 		} catch (err) {
 			console.log(err);
 		}
